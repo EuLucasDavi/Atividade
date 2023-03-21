40bac6f107b88d0a9c04ef55e339113e46a40e79
EuLucasDavi <lucas._.davi@hotmail.com>
2023-03-21 18:35:55 -0300

    new file:   farway/html/Imagem 2.jpeg new file:   farway/html/cadastro.html new file:   farway/html/cursos.html new file:   farway/html/estudo.png new file:   farway/html/index.html new file:   farway/html/sobre.html new file:   farway/instr.js new file:   farway/voltar.js

---
+++ farway/instr.js
@@ -0,0 +1,13 @@
+function createButton()
+{
+     var btn = document.createElement('BUTTON');
+     var lbl = document.createTextNode("CLICK ME");        
+     btn.appendChild(lbl);   
+     btn.onclick = function()
+     {
+        window.history.go(0);
+     }
+     document.body.appendChild(btn);    
+}
+
+createButton();
